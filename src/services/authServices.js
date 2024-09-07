d69e07bd9848d17a6b8a328811c905222d1037b4
PraveenKumar263 <PraveenKumar263>
2024-09-07 17:13:30 +0800

    update api path

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -2,16 +2,16 @@ import instance from "./instance";
 
 const authServices = {
     register: async (data) => {
-        return await instance.post('/registerUser', data);
+        return await instance.post('/api/v1/registerUser', data);
     },
     login: async (data) => {
-        return await instance.post('/loginUser', data);
+        return await instance.post('/api/v1/loginUser', data);
     },
     forgotPassword: async (email) => {
-        return await instance.put('/forgotPassword', { email });
+        return await instance.put('/api/v1/forgotPassword', { email });
     },
     resetPassword: async (token, newPassword) => {
-        return await instance.put(`/resetPassword/${token}`, { newPassword });
+        return await instance.put(`/api/v1/resetPassword/${token}`, { newPassword });
     }
 }
 
